66639e2971207b3787927d25484ff8de045292ec
thrinithomas <thomas060142@gmail.com>
2016-11-08 22:14:32 +0800

    upload by Android Studio

--- app/src/main/java/com/blockb/emmse/myapplication/Question2.java
+++ app/src/main/java/com/blockb/emmse/myapplication/Question2.java
@@ -1,7 +1,10 @@
 package com.blockb.emmse.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Question2 extends Activity {
 
@@ -9,5 +12,24 @@ public class Question2 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question2);
+
+        Button start = (Button)findViewById(R.id.submit);
+        start.setOnClickListener(
+                new android.widget.Button.OnClickListener(){
+                    public void onClick(View v){
+                        //Task 1: Get the date and season and do validation
+
+                        //After validation
+                        Intent intent = new Intent();
+                        //pass to next activity
+                        intent.setClass(Question2.this, Question3.class);
+                        startActivity(intent);
+                        Question2.this.finish();
+
+                    }
+                }
+        );
     }
+
+
 }
--- app/src/main/java/com/blockb/emmse/myapplication/Question3.java
+++ app/src/main/java/com/blockb/emmse/myapplication/Question3.java
@@ -1,7 +1,10 @@
 package com.blockb.emmse.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Thomas on 11/6/2016.
@@ -13,5 +16,23 @@ public class Question3 extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question3);
+
+        Button start = (Button)findViewById(R.id.submit);
+        start.setOnClickListener(
+                new android.widget.Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        //Task 1: Get the date and season and do validation
+
+                        //After validation
+                        Intent intent = new Intent();
+                        //pass to next activity
+                        intent.setClass(Question3.this, Question3.class);
+                        startActivity(intent);
+                        Question3.this.finish();
+
+                    }
+                }
+        );
     }
 }
